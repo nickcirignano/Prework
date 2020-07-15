ffaa7eece6ee914150b7bd9ebc600fa80f1b3549
Nick Cirignano <nickcirignano1@gmail.com>
2020-07-14 21:26:19 -0400

    First commit

---
+++ Module-6/javascript.js
@@ -0,0 +1,27 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = "Blue";
+
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity = "0.5";
+
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px"
+    document.getElementById("box").style.backgroundColor = "orange"
+    document.getElementById("box").style.opacity = "1";
+
+});
+
+
